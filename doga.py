a4ac3b784a01bbfb0027a907f3587e696410f8d1
RLevente01 <144986695+RLevente01@users.noreply.github.com>
2023-10-02 08:16:58 +0200

    Add files via upload

---
+++ doga.py
@@ -0,0 +1,25 @@
+szam=int(input("Kérek egy számot: "))
+szam2=int(input("Kérek egy számot: "))
+szam3=int(input("Kérek egy számot: "))
+szam4=int(input("Kérek egy számot: "))
+szam5=int(input("Kérek egy számot: "))
+
+import random
+
+print(random.randint(0,91))
+
+import random
+
+print(random.randint(0,91))
+
+import random
+
+print(random.randint(0,91))
+
+import random
+
+print(random.randint(0,91))
+
+import random
+
+print(random.randint(0,91))
